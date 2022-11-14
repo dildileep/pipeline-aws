beb5c722a8e1371315faf1b5ce839fb6174dd4ba
dileep kumar Sathyanarayana <dsathyanaray@marvell.com>
2022-11-14 17:13:55 +0530

    including requirements for the aws app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. SPDX-License-Identifier: MIT-0
 
 #!/usr/bin/env python3
 
@@ -11,10 +12,10 @@ class AwsEnvParity(core.App):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
 
-            self.stack_name = "yolo-application"
+            self.stack_name = "diltry1-app"
             self.base_module = CdkVpcStack(self, self.stack_name + "-base")
             self.pipeline_module = CdkPipelineStack(self, self.stack_name + "-pipeline", self.base_module.vpc)
-            
+
 if __name__ == '__main__':
     app = AwsEnvParity()
     app.synth()
